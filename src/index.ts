857f16d81c307699f715ebc46ae120f8851c636a
Jacky Jiang <t83714@gmail.com>
2020-07-27 22:56:43 +1000

    support GET request as well

--- src/index.ts
+++ src/index.ts
@@ -42,12 +42,33 @@ function handleError(e: Error, res?: Response) {
     }
 }
 
+/**
+ * This function can be used in the following ways:
+ * - Served by a HTTP server:
+ *   - GET request: `recordId` parameter should be passed via query string
+ *   - POST request: `recordId` parameter should be passed via query string or plain text in body (avaiable as function `input` parameter)
+ *   - Response through `res`
+ * - Served in CGI mode or run as commandline.
+ *   - `req` & `res` parameter will not be available
+ *   - `recordId` should be provided via `process.stdin`
+ *   - Output through `stdout`
+ *
+ * @export
+ * @param {*} input
+ * @param {Request} [req]
+ * @param {Response} [res]
+ */
 export default async function myFunction(
     input: any,
     req?: Request,
     res?: Response
 ) {
     try {
+        if (req?.query?.recordId) {
+            // function is served by http server and GET request
+            input = req.query.recordId;
+        }
+
         if (typeof input !== "string") {
             throw new Error("Expect record ID as input.");
         }
